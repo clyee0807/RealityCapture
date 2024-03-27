b61e96f9933aba7f17dc5c2872f0764f56eacb01
lychen <happying910807@gmail.com>
2024-03-27 21:16:50 +0800

    Initial commit

---
+++ RealityCapture/RealityCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityCaptureApp.swift
+//  RealityCapture
+//
+//  Created by CGVLAB on 2024/3/27.
+//
+
+import SwiftUI
+
+@main
+struct RealityCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
